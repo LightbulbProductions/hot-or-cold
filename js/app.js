f820b10e610f1f1cf238408f59cec88ae5453fc6
lightbulbproductions <gerbermichelle23@gmail.com>
2015-10-14 21:45:41 +0000

    adding js with unhappy global variables

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function(){
+$(document).ready(function() {
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -12,9 +12,19 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 });
+
 //Secret number computed between 1-100
-//Declare the global vars
+//Keeps bothering me about the global var grrr
   var secretNumber = Math.floor(Math.random()*100); 
   var userGuess;
-  
-
+  //The new game button
+var newGame = function() {
+		setSecretNumber();
+  var setSecretNumber = function() {
+  		secretNumber = Math.floor(Math.random()*100);
+  	};	
+};
+  var setUserGuess = function() {
+  		userGuess = $("#userGuess").val();
+  		console.log(userGuess);
+  	};
