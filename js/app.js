c8b0352e8d4df471b6a9980bdb61f168d1984cd9
lightbulbproductions <gerbermichelle23@gmail.com>
2015-10-14 21:22:49 +0000

    first add to js

--- js/app.js
+++ js/app.js
@@ -11,7 +11,10 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-
 });
-
+//Secret number computed between 1-100
+//Declare the global vars
+  var secretNumber = Math.floor(Math.random()*100); 
+  var userGuess;
+  
 
