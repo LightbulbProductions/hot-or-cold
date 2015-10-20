c5b1d643add05cff64d252879be375cabbc63708
lightbulbproductions <gerbermichelle23@gmail.com>
2015-10-20 18:37:25 +0000

    complete redo to js

--- dist/all.js
+++ dist/all.js
@@ -1,6 +1,8 @@
-
 $(document).ready(function() {
-	
+   window.onload = function() {
+  document.getElementById("userGuess").focus();
+};
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,20 +13,82 @@ $(document).ready(function() {
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+	
+  	$("#guessButton").click(function(event) {
+  		feedback();
+  		addGuessToList();
+  		clearUserGuess(;)
+  		/*preventDefault();*/
+  	});
+
+  	
+  $(".new").click(function(event) {
+  	newGame();
+  	setSecretNumber();
+  });
+  	
 });
 
-//Secret number computed between 1-100
-//Keeps bothering me about the global var grrr
+  //Declare variable functions
+  var newGame;
   var secretNumber = Math.floor(Math.random()*100); 
   var userGuess;
+  var guessCount = 0;
+  var feedback;
+  var clearUserGuess;
   //The new game button
+  
 var newGame = function() {
 		setSecretNumber();
-  var setSecretNumber = function() {
-  		secretNumber = Math.floor(Math.random()*100);
-  	};	
 };
-  var setUserGuess = function() {
+  
+
+var setSecretNumber = function() {
+  		secretNumber = Math.floor(Math.random()*100);
+  		console.log("secret number: " + secretNumber);
+ };
+
+var setUserGuess = function() {
   		userGuess = $("#userGuess").val();
-  		console.log(userGuess);
-  	};
+  		  		console.log(userGuess);
+};
+
+var addGuessToList = function () {
+	$("#guessList").append("<li>"+userGuess+"</li>");
+};
+
+var clearUserGuess = function () {
+		$("#userGuess").val("");
+		console.log(userGuess);
+	};
+	
+var feedback = function() {
+		setUserGuess();
+	
+		var difference = Math.abs(userGuess-secretNumber);
+		var postFeedback = function(message) {
+			$('#feedback').text(message);
+		};
+		
+	    if (userGuess === secretNumber) {
+			postFeedback("You are correct!");
+			updateGuessInfo();
+			secretNumber = "guessed";
+		}
+		else if(difference >= 1 && difference <= 10) {
+			postFeedback("You're on fire!");
+		}
+		else if(difference >= 11 && difference <= 20) {
+			postFeedback("Ooh! Getting hotter!");
+		}
+		else if(difference >= 21 && difference <= 30) {
+			postFeedback("Hmm... Rather tepid. Whomp Whomp");
+		}
+		else if (difference >= 31 && difference <= 50) {
+			postFeedback("It's getting chilly over here!");
+		}
+		else {
+			postFeedback("Aww! Cold as a popsicle! ");
+		}
+	
+};
--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-
 $(document).ready(function() {
+ window.onload = function(){
+    document.getElementById("userGuess").focus();
+    guessCount = 0;
+    initGuessList();
+};
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -11,20 +15,95 @@ $(document).ready(function() {
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+	
+  	$("input#guessButton").click(function(event) {
+  		event.preventDefault();
+  		feedback();
+  		addGuessToList();
+  		
+  	});
+  var initGuessList = function() {
+  $("span#count").text('0');
+  
+  };
+  
+  $(".new").click(function(event) {
+  	newGame();
+  	setSecretNumber();
+  	initGuessList();
+  });
+  	
 });
 
-//Secret number computed between 1-100
-//Keeps bothering me about the global var grrr
+  //Declare variable functions
+  var newGame;
   var secretNumber = Math.floor(Math.random()*100); 
   var userGuess;
-  //The new game button
+  var guessCount;
+
+  var userCount;
+  var feedback;
+  var guessCountBox;
+  var clearUserGuess;
+  
+  
 var newGame = function() {
 		setSecretNumber();
-  var setSecretNumber = function() {
-  		secretNumber = Math.floor(Math.random()*100);
-  	};	
+		guessCount = 0;
+		clearUserGuess();
 };
-  var setUserGuess = function() {
+
+
+var setSecretNumber = function() {
+  		secretNumber = Math.floor(Math.random()*100);
+  		console.log("secret number: " + secretNumber);
+ };
+
+var setUserGuess = function() {
   		userGuess = $("#userGuess").val();
-  		console.log(userGuess);
-  	};
+  		  		console.log(userGuess);
+};
+
+var addGuessToList = function () {
+	$("#guessList").append("<li>"+userGuess+"</li>");
+    $("span#count").text($("ul#guessList > li").size());
+    $("#userGuess").val('');
+    
+};
+
+var clearUserGuess = function () {
+		$("#userGuess").val("");
+		$("ul#guessList").empty();
+		$("#feedback").text('Make your Guess!');
+	};
+	
+var feedback = function() {
+		setUserGuess();
+	
+		var difference = Math.abs(userGuess-secretNumber);
+		var postFeedback = function(message) {
+			$('#feedback').text(message);
+		};
+		
+	    if (userGuess === secretNumber) {
+			postFeedback("You are correct!");
+	
+			secretNumber = "guessed";
+		}
+		else if(difference >= 1 && difference <= 10) {
+			postFeedback("You're on fire!");
+		}
+		else if(difference >= 11 && difference <= 20) {
+			postFeedback("Ooh! Getting hotter!");
+		}
+		else if(difference >= 21 && difference <= 30) {
+			postFeedback("Hmm... Rather tepid. Whomp Whomp");
+		}
+		else if (difference >= 31 && difference <= 50) {
+			postFeedback("It's getting chilly over here!");
+		}
+		else {
+			postFeedback("Aww! Cold as a popsicle! ");
+		}
+	
+};
